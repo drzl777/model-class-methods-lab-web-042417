b8677612b873dfc044012490e1e7632a5b5f98da
drzly777 <andrew.zheng725@gmail.com>
2017-05-05 13:58:52 -0400

    finished all queries but may not have done it the correct ar way

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,36 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    self.all.limit(5)
+  end
+
+  def self.dinghy
+    self.all.where("length < ?", 20)
+  end
+
+  def self.ship
+    self.all.where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    self.all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    self.all.where("captain_id IS NULL")
+  end
+
+  def self.sailboats
+    Classification.all.find_by(name: "Sailboat").boats
+  end
+
+  def self.with_three_classifications
+    #Boat.all.having('COUNT(boat.classifications) > 3')
+    #binding.pry
+    self.all.joins(:boat_classifications).group('boats.id').having('COUNT(boats.id) >= 3')
+  end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,33 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    #inding.pry
+    captain_ids = Classification.all.find_by(name: "Catamaran").boats.pluck(:captain_id) #return all captains
+    #self.all.where("")
+    self.all.where(id: captain_ids)
+  end
+
+  def self.sailors
+    captain_ids = Boat.sailboats.pluck(:captain_id)
+    self.all.where(id: captain_ids)
+  end
+
+  def self.talented_seamen
+    #binding.pry
+    sailor_ids = Boat.sailboats.pluck(:captain_id).uniq
+    motor_ids = Classification.all.find_by(name: "Motorboat").boats.pluck(:captain_id).uniq
+    both_ids = []
+    sailor_ids.each {|id| both_ids << id if motor_ids.include?(id)}
+    self.all.where(id: both_ids)
+  end
+
+
+  def self.non_sailors
+    sailor_ids = Boat.sailboats.pluck(:captain_id).uniq
+    all_capt_ids = Boat.all.pluck(:captain_id).uniq
+    non_sailor_ids = all_capt_ids.reject {|c_id| sailor_ids.member?(c_id)}
+    self.all.where(id: non_sailor_ids)
+  end
+
 end
